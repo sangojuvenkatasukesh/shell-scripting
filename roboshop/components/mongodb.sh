ff556187d7b9df144d2819ea615ee6a45efe0f94
sukesh <you@example.com>
2021-09-14 08:45:59 +0000

    pushing 2 changes to mongodb

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -7,7 +7,7 @@ gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 
-yum install -y mongodb-org 
+yum install -y mongodb-org  >/tmp/log
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 
@@ -18,7 +18,7 @@ systemctl restart mongod
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 
 cd /tmp
-unzip mongodb.zip
+unzip -o mongodb.zip
  cd mongodb-main
  mongo < catalogue.js
  mongo < users.js 

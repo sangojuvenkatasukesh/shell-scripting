315901ce883080b1167b0c89228d8922914a2705
sukesh <you@example.com>
2021-09-14 14:05:27 +0000

    Lastest update in mongodb

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -14,7 +14,7 @@ sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 echo "Starting Mongodb"
 systemctl enable mongod
 systemctl restart mongod
-if [ $? -eq 0];then
+if [ $? -eq 0 ];then
     echo -e "\e[32mSUCCESS\e[0m"
   else
   echo -e "\e[31mFAILURE\e[0m"
@@ -22,7 +22,7 @@ fi
 
 echo "Downloading Mongodb"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
-if [ $? -eq 0];then
+if [ $? -eq 0 ];then
     echo -e "\e[32mSUCCESS\e[0m"
   else
   echo -e "\e[31mFAILURE\e[0m"
@@ -30,7 +30,7 @@ fi
 cd /tmp
 echo "Extracting schema Archive"
 unzip -o mongodb.zip &>>/tmp/log
-if [ $? -eq 0];then
+if [ $? -eq 0 ];then
     echo -e "\e[32mSUCCESS\e[0m"
   else
   echo -e "\e[31mFAILURE\e[0m"
@@ -39,7 +39,7 @@ fi
  echo "Loading schema"
  mongo < catalogue.js &>>/tmp/log
  mongo < users.js  &>>/tmp/log
-if [ $? -eq 0];then
+if [ $? -eq 0 ];then
     echo -e "\e[32mSUCCESS\e[0m"
   else
   echo -e "\e[31mFAILURE\e[0m"

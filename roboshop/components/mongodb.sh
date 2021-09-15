4c881e0787537c4484019ce35a5e7795cbe7e78d
sukesh <you@example.com>
2021-09-15 03:16:25 +0000

    Added new functions

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,3 +1,12 @@
+#!bin/bash
+Status_Check() {
+if [ $? -eq 0 ];then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 2
+fi    
+}
 echo "Setting up Mongodb repo"
 
 echo '[mongodb-org-4.2]
@@ -6,57 +15,28 @@ baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
-if [ $? -eq 0 ];then
-    echo -e "\e[32mSUCCESS\e[0m"
-  else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
+Status_Check $?
 echo "Installing Mongodb"
 yum install -y mongodb-org  &>>/tmp/log
-if [ $? -eq 0 ];then
-    echo -e "\e[32mSUCCESS\e[0m"
-  else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
+Status_Check $?
 echo "Configuring Mongodb"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-if [ $? -eq 0 ];then
-    echo -e "\e[32mSUCCESS\e[0m"
-  else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
+Status_Check $?
 
 echo "Starting Mongodb"
 systemctl enable mongod
 systemctl restart mongod
-if [ $? -eq 0 ];then
-    echo -e "\e[32mSUCCESS\e[0m"
-  else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
+Status_Check $?
 
 echo "Downloading Mongodb"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
-if [ $? -eq 0 ];then
-    echo -e "\e[32mSUCCESS\e[0m"
-  else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
+Status_Check $?
 cd /tmp
 echo "Extracting schema Archive"
 unzip -o mongodb.zip &>>/tmp/log
-if [ $? -eq 0 ];then
-    echo -e "\e[32mSUCCESS\e[0m"
-  else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
+Status_Check $?
  cd mongodb-main
  echo "Loading schema"
  mongo < catalogue.js &>>/tmp/log
  mongo < users.js  &>>/tmp/log
-if [ $? -eq 0 ];then
-    echo -e "\e[32mSUCCESS\e[0m"
-  else
-  echo -e "\e[31mFAILURE\e[0m"
-fi
-
+Status_Check $?

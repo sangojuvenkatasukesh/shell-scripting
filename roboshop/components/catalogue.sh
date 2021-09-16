432262481b60c38c7bc282d00600b93c94bfe3eb
sukesh <you@example.com>
2021-09-16 05:30:06 +0000

    new changes in catalogue

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,23 +1,25 @@
 #!bin/bash
+
+
 source components/common.sh
 
 Print "Installing NodeJS"
-yum install nodejs make gcc-c++ -y  &>>$LOG
-Status_check $?
+yum install nodejs make gcc-c++ -y &>>$LOG
+Status_Check $?
 
 Print "Adding roboshop user"
 useradd roboshop &>>$LOG
-Status_check $?
+Status_Check $?
 
 Print "Downloading Catalogue Content"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
-Status_check $?
+Status_Check $?
 
 Print "Extracting Catalogue"
 cd /home/roboshop
 unzip -o /tmp/catalogue.zip &>>$LOG
 mv catalogue-main catalogue
-Status_check $?
+Status_Check $?
 
 cd /home/roboshop/catalogue
 npm install  &>>$LOG

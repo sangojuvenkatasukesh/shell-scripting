f728b94a364cf2dd798f0ce40f80b93ceeca1917
sukesh <you@example.com>
2021-09-16 05:01:54 +0000

    New catallogue added

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,2 +1,29 @@
 #!bin/bash
-echo "Installing catalogue"
+source components/common.sh
+
+Print "Installing NodeJS"
+yum install nodejs make gcc-c++ -y 
+Status_check $?
+
+Print "Adding roboshop user"
+useradd roboshop &>>$LOG
+Status_check $?
+
+Print "Downloading Catalogue Content"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+Status_check $?
+
+Print "Extracting Catalogue"
+cd /home/roboshop
+unzip -o /tmp/catalogue.zip &>>$LOG
+mv catalogue-main catalogue
+Status_check $?
+
+cd /home/roboshop/catalogue
+npm install  &>>$LOG
+
+
+# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+# systemctl daemon-reload
+# systemctl start catalogue
+# systemctl enable catalogue

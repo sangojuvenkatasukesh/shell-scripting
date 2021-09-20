09f26c5526ce580155c22f90752bb9d05f71224d
sukesh <you@example.com>
2021-09-20 09:10:10 +0000

    New added redis

---
+++ roboshop/components/redis.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+
+source components/common.sh
+
+
+Print "Install Yum Utils & Download redis repo"
+yum install epel-release http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y  &>>$LOG
+ Status_Check $?
+ 
+Print "Setup Redis Repo"
+yum-config-manager --enable remi &>>$LOG
+Status_Check $?
+Print " Install Redis"
+yum install redis -y  &>>$LOG
+Status_Check $?
+Print "Configure Redis Listen Address"
+sed -i -e's/127.0.0.1/ 0.0.0.0/'  /etc/redis.conf 
+
+Print "Start Redis Service"
+
+ systemctl enable redis   &>>$LOG   && systemctl start redis  &>>$LOG

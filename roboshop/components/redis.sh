e0dc8e20023380f250f886c7c56f034b7f997045
sukesh <you@example.com>
2021-09-21 04:31:49 +0000

    New changes in redis

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -5,10 +5,10 @@ source components/common.sh
 
 Print "Install Yum Utils & Download redis repo"
 yum install epel-release http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y  &>>$LOG
- Status_Check $?
+Status_Check $?
  
 Print "Setup Redis Repo"
-yum-config-manager --enable remi &>>$LOG
+yum-config-manager --enable remi  &>>$LOG
 Status_Check $?
 
 Print " Install Redis"

fa80ce92e8b018a2485ccb59e6d57b0083647c97
sukesh <you@example.com>
2021-09-21 04:56:51 +0000

    New changes in redis

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -1,24 +1,22 @@
 #!bin/bash
 
 source components/common.sh
-
-
-Print "Install Yum Utils & Download redis repo"
-yum install epel-release http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y  &>>$LOG
-Status_Check $?
+Print "Install Yum  Utils & Download Redis Repo"
+yum install epel-release yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>$LOG
+ Status_Check $?
  
-Print " Setup Redis Repo"
+  Print "Setup Redis repo"
 yum-config-manager --enable remi &>>$LOG
-Status_Check $?
+ Status_Check $?
 
-Print " Install Redis"
-yum install redis -y  &>>$LOG
-Status_Check $?
+Print  "Install Redis"
+ yum install redis -y &>>$LOG
+ Status_Check $?
+ 
+ Print "Configure Redis Listen Address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
+ Status_Check $?
 
-Print "Configure Redis Listen Address"
-sed -i -e's/127.0.0.1/ 0.0.0.0/'  /etc/redis.conf 
-Status_Check $?
-
-Print "Start Redis Service"
-systemctl enable redis &>>$LOG && systemctl start redis  &>>$LOG
+Print "Start Database"
+systemctl enable redis  &>>$LOG && systemctl start redis  &>>$LOG
 Status_Check $?

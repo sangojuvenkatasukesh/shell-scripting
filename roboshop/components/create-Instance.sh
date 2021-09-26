3be3268a96f55963252f51addb60e5b219142488
sukesh <you@example.com>
2021-09-26 10:19:30 +0530

    --- Auto-Git Commit ---

--- roboshop/components/create-Instance.sh
+++ roboshop/components/create-Instance.sh
@@ -4,5 +4,5 @@ LID="lt-07a997b45f50e990a"
 LVER=1
 INSTANCE_NAME=$1
 
-if [ -z ${INSTANCE_NAME}]
+if [ -z "${INSTANCE_NAME}]
 InstanceID=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications  "ResourceType= spot-instances-request,Tags=[{Key=Name,Value=string}]"| jq .Instances[].InstanceId sed -e's/"//g'

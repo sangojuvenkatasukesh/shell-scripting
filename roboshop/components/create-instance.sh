3722fb2402fc9c079f6cf2a06d7b67e9c6099ba3
sukesh <you@example.com>
2021-09-26 14:18:37 +0530

    --- Auto-Git Commit ---

--- roboshop/components/create-instance.sh
+++ roboshop/components/create-instance.sh
@@ -10,6 +10,10 @@ if [ -z "${INSTANCE_NAME}" ]; then
 fi
 
 
-a
+aws ec2 describe-instances --filters "Name=tag:Name,Values=$INSTANCE_NAME" | jq .Reservations[].Instances[].State.Name | grep running &>/dev/null
+if [ $? -eq 0 ]; then
+  echo "Instance $INSTANCE_NAME is already running"
+  exit 0
+fi
 
 InstanceID=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')

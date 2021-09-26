c4039d5545129246ed1809cdddc01dd677d903eb
sukesh <you@example.com>
2021-09-26 10:09:18 +0530

    --- Auto-Git Commit ---

--- roboshop/components/create-Instance.sh
+++ roboshop/components/create-Instance.sh
@@ -3,4 +3,4 @@
 LID="lt-07a997b45f50e990a"
 LVER=1
 
-aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications  | jq .Instances[].InstanceId
+aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications  ResourceType=string,Tags=[{Key=string,Value=string},{Key=string,Value=string}] | jq .Instances[].InstanceId

2c40feceebf05debd4cbb2b5e2dcc420c05bb525
sukesh <you@example.com>
2021-09-26 10:56:52 +0530

    --- Auto-Git Commit ---

--- roboshop/components/create-Instance.sh
+++ roboshop/components/create-Instance.sh
@@ -7,4 +7,5 @@ if [ -z "${INSTANCE_NAME}" ]; then
   echo "Input is missing"
   exit 1
 fi
+InstanceID=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
 

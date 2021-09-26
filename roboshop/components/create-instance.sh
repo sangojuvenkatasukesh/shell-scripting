0cce26d96e52e94cb981d868d778fbe6382217bc
sukesh <you@example.com>
2021-09-26 17:28:11 +0530

    --- Auto-Git Commit ---

--- roboshop/components/create-instance.sh
+++ roboshop/components/create-instance.sh
@@ -23,5 +23,3 @@ fi
 
 InstanceID=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
 
-sed -e "s/INSTANCE_NAME/$INSTANCE_NAME/" -e "s/INSTANCE_IP/$IP/" record.json  >/tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id Z09419003SDE5CZ2II0X0 --change-batch file:///tmp/record.json | jq

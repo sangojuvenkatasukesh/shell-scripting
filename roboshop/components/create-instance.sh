b70882fa606483808d2c6e16a2b6bc8fd08f113c
sukesh <you@example.com>
2021-09-26 17:04:25 +0530

    --- Auto-Git Commit ---

--- roboshop/components/create-instance.sh
+++ roboshop/components/create-instance.sh
@@ -22,3 +22,5 @@ if [ $? -eq 0 ]; then
 fi
 
 InstanceID=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
+
+sed -

8003017db3d2218ef81936715983f046e76fd534
sukesh <you@example.com>
2021-09-27 16:04:17 +0530

    --- Auto-Git Commit ---

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -19,6 +19,6 @@ Print "Update Roboshop Config File"
 Status_Check $?
 
 
-Print "Starting Nginx"
+Print "Starting Nginx\t\t"
 systemctl restart nginx  &>>$LOG  &&  systemctl enable nginx  &>>$LOG
 Status_Check $?

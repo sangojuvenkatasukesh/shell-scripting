1c6ef8f68dbf075f08c2d6fcae9b80cd0829ed55
sukesh <you@example.com>
2021-09-27 18:37:20 +0530

    --- Auto-Git Commit ---

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -14,7 +14,7 @@ Print  "Install Redis\t\t\t\t"
  Status_Check $?
  
  Print "Configure Redis Listen Address\t\t"
- if [ -f  /etc/redis.conf]
+ if [ -f  /etc/redis.conf ]; then
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
  Status_Check $?
 

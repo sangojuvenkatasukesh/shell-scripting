1bc637752f6911c1ad6fee867c1773dc7c194483
sukesh <you@example.com>
2021-09-27 18:37:35 +0530

    --- Auto-Git Commit ---

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -17,6 +17,7 @@ Print  "Install Redis\t\t\t\t"
  if [ -f  /etc/redis.conf ]; then
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
  Status_Check $?
+ 
 
 Print "Start Database\t\t\t\t"
 systemctl enable redis  &>>$LOG && systemctl start redis  &>>$LOG

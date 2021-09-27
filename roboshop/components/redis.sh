7728b7b636929eac4e789d69e8afd92206b3efa5
sukesh <you@example.com>
2021-09-27 18:39:02 +0530

    --- Auto-Git Commit ---

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -18,7 +18,7 @@ Print  "Install Redis\t\t\t\t"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
 fi
  if [ -f /etc/redis/redis.conf ]; then
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
  Status_Check $?
 
 Print "Start Database\t\t\t\t"

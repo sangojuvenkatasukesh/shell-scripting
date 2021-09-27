fa106f012d352423a7d4744c28e104ac28c303c7
sukesh <you@example.com>
2021-09-27 16:05:46 +0530

    --- Auto-Git Commit ---

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,7 +6,7 @@ source components/common.sh
 yum install nginx -y &>>$LOG
 Status_Check $?
 
-Print "DownLoad Frontend Archive\t"
+Print "DownLoad Frontend Archive\t\t"
  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 Status_Check $?
 
@@ -14,7 +14,7 @@ Print "Extract Frontend Archive\t"
 rm -rf /usr/share/nginx/* && cd /usr/share/nginx && unzip -o /tmp/frontend.zip  &>>$LOG  && mv frontend-main/* .  &>>$LOG  &&   mv static html  &>>$LOG
 Status_Check $?
 
-Print "Update Roboshop Config File"
+Print "Update Roboshop Config File\t"
  mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
 Status_Check $?
 

0c06630bc7e1956dd071935c6498a05e0fff0bdf
sukesh <you@example.com>
2021-11-03 22:21:12 +0530

    --- Auto-Git Commit ---

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -8,7 +8,7 @@ Status_Check $?
 
 Print "DownLoad Frontend Archive\t"
  curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-Status_Check /etc/$?
+Status_Check $?
 
 Print "Extract Frontend Archive\t"
 rm -rf /usr/share/nginx/* && cd /usr/share/nginx && unzip -o /tmp/frontend.zip  &>>$LOG  && mv frontend-main/* .  &>>$LOG  &&   mv static html  &>>$LOG

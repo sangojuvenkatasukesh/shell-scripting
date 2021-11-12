c6badb08fc5e2c399fd3cef5d1480a8bbccb88f9
sukesh <you@example.com>
2021-11-12 19:04:31 +0530

    --- Auto-Git Commit ---

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -22,7 +22,7 @@ DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print
 )
 
 echo  'ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';'  >/tmp/reset.mysql
-mysql -u root -p"{DEFAULT_PASSWORD}" </tmp/reset.mysql
+mysql -u root -p"{DEFAULT_PASSWORD}"  </tmp/reset.mysql
 
 exit 
 

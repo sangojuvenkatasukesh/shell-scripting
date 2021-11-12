68dd4f2013b3e3a61f16033f3ce6fc77c0bdbfd4
sukesh <you@example.com>
2021-11-12 17:59:03 +0530

    --- Auto-Git Commit ---

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -11,8 +11,7 @@ gpgcheck=0' > /etc/yum.repos.d/mysql.repo
 Status_Check $?
 
 Print "Install MySQL Service"
-yum remove mariadb-libs -y  &>>$LOG
-Status_Check $? &&  yum install mysql-community-server -y   &>>$LOG
+yum remove mariadb-libs -y  &>>$LOG &&  yum install mysql-community-server -y   &>>$LOG
 Status_Check $?
 
 Start MySQL.

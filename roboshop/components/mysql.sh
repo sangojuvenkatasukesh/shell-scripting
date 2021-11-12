3952b69582bf2352545cdecb43af2b085a2d8af4
sukesh <you@example.com>
2021-11-12 18:14:35 +0530

    --- Auto-Git Commit ---

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -2,7 +2,7 @@
 source components/common.sh
 
 Print "Setup MySql Repo"
-Setup MySQL Repo
+Setup
 # echo '[mysql57-community]
 name=MySQL 5.7 Community Server
 baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/

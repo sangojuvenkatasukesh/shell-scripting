884d8e856be50e91d122826b064efa4f6361733f
sukesh <you@example.com>
2021-11-12 15:10:45 +0530

    --- Auto-Git Commit ---

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -44,7 +44,6 @@ SystemdD_setup() {
 Print "Update Systemd Service\t\t\t"
 sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/'  /home/roboshop/ ${COMPONENT} /systemd.service
 Status_Check $?
-
 Print "Setup Systemd Service\t\t\t"
 mv /home/roboshop/ ${COMPONENT} /systemd.service  /etc/systemd/system/cat ${COMPONENT} alogue.service && systemctl daemon-reload && systemctl restart  ${COMPONENT}  &>>$LOG
 systemctl enable  ${COMPONENT}   &>>$LOG

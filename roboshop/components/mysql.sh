957cc74b1a2b8e43780ba080ba04f41caae933e3
sukesh <you@example.com>
2021-11-12 22:54:51 +0530

    --- Auto-Git Commit ---

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -26,7 +26,7 @@ echo 'show databases' |  mysql -u root -pRoboShop@1   &>>$LOG
 if [ $? -eq 0 ]; then
 echo "Password is already set"  &>>$LOG
 else 
-echo  "UPDATE mysql.user SET authentication_string = PASSWORD('new_password') '; "  >/tmp/reset.mysql
+echo  "UPDATE mysql.user SET authentication_string = PASSWORD'new_password'; "  >/tmp/reset.mysql
 mysql --connect-expired-password -u root -p"{DEFAULT_PASSWORD}"  </tmp/reset.mysql &>>$LOG
 fi
 Status_Check $?

9125352d16d6e23a6f265eaaf45808e3cdd0b229
sukesh <you@example.com>
2021-11-12 18:09:03 +0530

    --- Auto-Git Commit ---

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -15,8 +15,8 @@ yum remove mariadb-libs -y  &>>$LOG  &&  yum install mysql-community-server -y  
 Status_Check $?
 
 print "Start MySQL Service"
- systemctl enable mysqld   &>>$LOG && systemctl start mysqld
- Status_Check $?
+systemctl enable mysqld   &>>$LOG && systemctl start mysqld
+Status_Check $?
 
 Now a default root password will be generated and given in the log file.
 # grep temp /var/log/mysqld.log

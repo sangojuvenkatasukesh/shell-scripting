e266eafa4f40815dab064f93c857270e5338ee9b
sukesh <you@example.com>
2021-11-12 17:56:01 +0530

    --- Auto-Git Commit ---

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -8,6 +8,7 @@ name=MySQL 5.7 Community Server
 baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/
 enabled=1
 gpgcheck=0' > /etc/yum.repos.d/mysql.repo
+Status_Chwe
 
 Install MySQL
 # yum remove mariadb-libs -y 

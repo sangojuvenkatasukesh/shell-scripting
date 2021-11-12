320502fa368b180a99b1455bb2bd475da79ffb28
sukesh <you@example.com>
2021-11-12 21:27:56 +0530

    --- Auto-Git Commit ---

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -26,7 +26,7 @@ echo 'show databases' |  mysql -u root -pRoboShop@1   &>>$LOG
 if [ $? -eq 0 ]; then
 echo "Password is already set"  &>>$LOG
 else 
-echo  "mysql> CREATE USER 'root'@'localhost' IDENTIFIED BY 'password'; "  >/tmp/reset.mysql
+echo  "mysql> CREATE USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1 '; "  >/tmp/reset.mysql
 mysql --connect-expired-password -u root -p"{DEFAULT_PASSWORD}"  </tmp/reset.mysql &>>$LOG
 fi
 Status_Check $?

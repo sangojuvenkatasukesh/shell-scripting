5e6107cd73c14802d75c21e098aa8ae7954c73c0
sukesh <you@example.com>
2021-11-12 14:39:58 +0530

    --- Auto-Git Commit ---

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -31,7 +31,7 @@ Status_Check $?
 }
 DOWNLOAD() {
  Print "Downloading ${COMPONENT} Content\t\t"
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+curl -s -L -o /tmp/${COMPONENT} .zip "https://github.com/roboshop-devops-project/${COMPONENT} /archive/main.zip"
 Status_Check $?
 Print "Extracting ${COMPONENT} \t\t\t"
 cd /home/roboshop

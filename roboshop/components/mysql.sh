f27527ca072d489ba127133f2910177e99a1f34e
sukesh <you@example.com>
2021-11-12 22:41:27 +0530

    --- Auto-Git Commit ---

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -42,7 +42,7 @@ Status_Check $?
 
 Print "Download the Schema"
 curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>$LOG
-Status_
+Status_Check 
 
 # cd /tmp
 # unzip mysql.zip

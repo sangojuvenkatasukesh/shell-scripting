545f6c543ce89314a96d88cf4bacc9f81c43f12f
sukesh <you@example.com>
2021-11-13 14:31:46 +0530

    --- Auto-Git Commit ---

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -3,7 +3,7 @@ if [ $? -eq 0 ];then
     echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
-    echo -e "\e[33mRefer Log file : $LOG for more information\e[0m"
+    echo -e "\e[33m Refer Log file : $LOG for more information\e[0m"
     exit 2
 fi    
 }

018060cb4d34dbc25cce9bce574190dd93035c40
sukesh <you@example.com>
2021-11-13 18:23:58 +0530

    --- Auto-Git Commit ---

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -90,7 +90,7 @@ PYTHON() {
     
     DOWNLOAD
     
-    cd /home/roboshop/${COMPONENT}  
+    cd /home/roboshop/payment
 
     Print "Install Python Dependencies"
     pip3 install -r requirements.txt  &>>$LOG

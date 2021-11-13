4a8d70e68b7161252e886bd4d325927a1078ca6d
sukesh <you@example.com>
2021-11-13 14:30:03 +0530

    --- Auto-Git Commit ---

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -25,7 +25,7 @@ Status_Check $?
 
 
 Print "Create application user\t"
-rabbitmqctl list_users | grep roboshop  &>>$LOG  
+ rabbitmqctl list_users | grep roboshop  &>>$LOG  
 if [ $? -eq 0 ];then
     rabbitmqctl add_user roboshop roboshop123  &>>$LOG
 fi

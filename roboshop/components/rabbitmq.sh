01efca42ec19ee6db7597782e85929d11f238cc4
sukesh <you@example.com>
2021-11-13 13:49:29 +0530

    --- Auto-Git Commit ---

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -3,7 +3,7 @@
 source components/common.sh
 
 Print "Install Erlang\t\t\t"
-yum list installed | grep erlang   &>>$LOF
+yum list installed | grep erlang   &>>$LOG
 
 yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>$LOG
 Status_Check $?
